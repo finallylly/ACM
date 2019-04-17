f0a1d4a1b2c78c3534b23d33a2fa52a5fae1ba56
finallylly <347881230@qq.com>
2019-04-17 22:20:40 +0800

    66. Plus One

---
+++ Leetcode/Python3/#66_PlusOne_Easy.py
@@ -0,0 +1,28 @@
+```
+66. Plus One
+Easy
+
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+
+Example 2:
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+
+link: https://leetcode.com/problems/find-all-duplicates-in-an-array/
+```
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        num=0
+        for x in digits:
+            num = (num*10 + x)
+
+        return list(map(lambda x:int(x), list(str(num+1))))
