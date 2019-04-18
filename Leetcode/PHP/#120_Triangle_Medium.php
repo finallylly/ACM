bef9ea0109a1e3dfc018ef4203b4252593a4ebbf
zhiyuan.lin <zhiyuanlin@rastar.com>
2019-04-18 17:35:08 +0800

    120. Triangle

---
+++ Leetcode/PHP/#120_Triangle_Medium.php
@@ -0,0 +1,41 @@
+<?php
+/**
+120. Triangle
+Medium
+
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+For example, given the following triangle
+
+[
+    [2],
+   [3,4],
+  [6,5,7],
+ [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+Note:
+Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+
+link: https://leetcode.com/problems/triangle/
+ */
+
+class Solution {
+
+    /**
+     * @param Integer[][] $triangle
+     * @return Integer
+     */
+    function minimumTotal($triangle) {
+        if(empty($triangle)){ return 0; }
+
+        $height = count($triangle);
+        for($i=$height-1;$i>0;$i--){
+            $width = count($triangle[$i]);
+            for($j=0;$j<$width-1;$j++){
+                $triangle[$i-1][$j] += min($triangle[$i][$j], $triangle[$i][$j+1]);
+            }
+        }
+        return $triangle[0][0];
+    }
+}
